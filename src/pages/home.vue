150812a3dcc84b04f35ca5aad7ac9cd925525c91
heuulzp <heuulzp@163.com>
2017-09-05 16:34:12 +0800

    mock生成数据

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -6,44 +6,55 @@
           <h1>豆瓣</h1>
         </div>
       </div>
-      <ul>
-        <li>
-          <div class="mui-card">
-    				<!--页眉，放置标题-->
-    				<div class="mui-card-header mui-card-media">
-    					<img src="http://a3.att.hudong.com/48/45/01100000000000144728459490363_s.jpg" />
-    					<div class="mui-media-body">
-    						小黄人
-    						<p>发表于 2017-09-01 15:00</p>
-    					</div>
-    				</div>
-    				<!--内容区-->
-    				<div class="mui-card-content">
-    					<div class="custom-content">
-    						<img src="http://a3.att.hudong.com/48/45/01100000000000144728459490363_s.jpg" alt="avator" />
-    					</div>
-    				</div>
-    				<!--页脚，放置补充信息或支持的操作-->
-    				<div class="mui-card-footer">LIKE</div>
-    			</div>
-        </li>
-      </ul>
+      <div class="container">
+        <ul>
+          <li class="item" v-for="item in bookList">
+            <div class="cover">
+              <img :src=item.image :alt=item.bookname class="img-show">
+            </div>
+            <div class="info">
+              <h4></h4>
+              <h3>{{item.bookname}}</h3>
+              <p class="meta">{{item.author}}</p>
+              <p class="meta">{{item.info}}</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+
     </header>
   </div>
 </template>
 
 <script>
+import Mock from 'mockjs'
+
 export default {
   name: 'home',
   data () {
     return {
+      bookList: []
     }
+  },
+  mounted: function () {
+    this.bookList = Mock.mock({
+      'list|20': [{
+        'id|+1': 1,
+        'bookname': '@ctitle',
+        'author': '@cname',
+        'info': '@cparagraph',
+        'image': '@image("200x300", "#ffcccc")'
+      }]
+    }).list
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+  /**
+   * header部分
+   */
   header {
     position: fixed;
     left: 0;
@@ -63,4 +74,48 @@ export default {
       color: #42b983;
     }
   }
+  /**
+   * 内容区域
+   */
+  .item {
+    padding: 20px 4%;
+    overflow: hidden;
+    position: relative;
+    .cover {
+      float: left;
+      margin-right: 15px;
+      width: 100px;
+      min-height: 100px;
+      .img-show {
+        opacity: 1;
+      }
+      img {
+        width: 100%;
+        opacity: 0;
+        transition: opacity 0.3s ease-out;
+      }
+    }
+    .info {
+      overflow: hidden;
+      padding-top: 5px;
+      h3 {
+        font-size: 16px;
+        color: #494949;
+      }
+      .meta {
+        font-size: 12px;
+        margin: 0;
+      }
+    }
+  }
+  .item::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    height: 1px;
+    background: #ccc;
+  }
+
 </style>
